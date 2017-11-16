c3b271186de018b22b165af84f8a47b53ecb592f
Kimura <joseduardo.kimura@gmail.com>
2017-11-16 14:02:33 -0200

    :repeat: Add user avatar directory settings

--- djlp/settings.py
+++ djlp/settings.py
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# DJLP custom settings
+USER_AVATAR_DIR = os.path.join(BASE_DIR, 'user_avatar')
